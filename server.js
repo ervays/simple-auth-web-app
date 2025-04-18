7cf0de29b5c773081955ec2b85cc16fbd530c43f
ervays <vicente.arnau2@gmail.com>
2025-04-18 19:37:56 +0200

    Update Content Security Policy to allow connections to remote API

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
-const API_URL = process.env.API_URL || 'http://localhost:5000';
+const API_URL = process.env.API_URL || 'http://13.51.234.173:5000'; // Updated default to your remote API
 
 // Serve static files
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
 // Middleware to inject the API_URL environment variable
 app.use((req, res, next) => {
-  res.setHeader('Content-Security-Policy', "default-src 'self'; connect-src 'self' " + API_URL);
+  // More permissive CSP that allows connections to any origin
+  // This is fine for development but should be tightened for production
+  res.setHeader('Content-Security-Policy', "default-src 'self'; connect-src 'self' http://localhost:5000 http://13.51.234.173:5000 *");
   next();
 });
 
